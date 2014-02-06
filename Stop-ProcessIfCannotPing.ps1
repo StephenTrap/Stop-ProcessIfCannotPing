b68092fb7fe5e497a13dab381e77ac12c2a7ee8a
Stephen Trap <strapanese@highlandscsb.org>
2014-02-06 16:34:04 -0500

    Conditions and Log file
    Added conditions and output to a log file when process has to be
    stopped.

--- Stop-ProcessIfCannotPing.ps1
+++ Stop-ProcessIfCannotPing.ps1
@@ -5,12 +5,12 @@
 
 #This pings the firewall and returns a 'true' or 'false' response.
 $ping = test-connection 192.168.1.1 -count 1 -quiet
+$ProcessActive = Get-Process -Name OUTLOOK -ErrorAction SilentlyContinue
 
 #If the network is down the offending process is stopped. If network is up do nothing.
-if ($ping -eq $false)
-{
-    Stop-Process -Name OUTLOOK -Force
-}
 
-#Plans for improvement:
-## Add a note in a log file when the process has to be stopped.
+if ($ping -eq $false -and $ProcessActive -ne $null)
+{
+    Get-Date | Out-File C:\powershell\log.txt -Append
+    Stop-Process -Name OUTLOOK -Force -Verbose -PassThru | Out-File C:\powershell\log.txt -Append
+}
